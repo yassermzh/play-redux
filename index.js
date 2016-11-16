53bb6f003485a586488c7ff2deda014138eeadd9
Yasser <yassermzh@gmail.com>
2016-11-16 10:24:59 +0330

    add fetching from server: async action

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ import { createStore, applyMiddleware } from 'redux'
 
 // actions
 const ADD_TODO = 'ADD_TODO'
+const GET_TODO = 'GET_TODO'
+const GET_TODO_SUCCESS = 'GET_TODO_SUCCESS'
 
 // action creator
 const addTodo = (content) => ({ type: ADD_TODO, content })
+const getTodos = () => ({ type: GET_TODO })
+const getTodosSuccess = (todos) => ({ type: GET_TODO_SUCCESS, todos })
+
+// mock fetch
+const fetch = () => { return Promise.resolve(['this', 'that']) }
+
+const fetchTodos = (store) => {
+  store.dispatch(getTodos())
+  return fetch('/api/todos')
+    .then(todos => {
+      store.dispatch(getTodosSuccess(todos))
+    })
+}
 
 const initialValue = { todos: [] }
 
@@ -12,6 +27,12 @@ const initialValue = { todos: [] }
 const reducer = (state = initialValue, action) => {
   if (action.type == ADD_TODO)
     return { todos: [...state.todos, action.content] }
+  else if (action.type == GET_TODO)
+    return { fetching: true }
+  else if (action.type == GET_TODO_SUCCESS)
+    return { todos: action.todos, fetching: false }
+  else
+    return state
 }
 
 const logger = store => next => action => { // eslint-disable-line no-unused-vars
@@ -25,5 +46,8 @@ store.subscribe(() => {
   console.log('store\'s state=', store.getState())
 })
 
-store.dispatch(addTodo('do this'))
-store.dispatch(addTodo('do that'))
+fetchTodos(store)
+  .then(() => {
+    store.dispatch(addTodo('do this'))
+    store.dispatch(addTodo('do that'))
+  })
